898017616507d20b29c92a7d500fee85902d98d0
Martin <souzam@sonic.net>
2023-12-21 12:59:13 -0500

    Progress implementing custom MUI theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import CssBaseline from "@mui/material/CssBaseline"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { Container } from "@mui/material"
 import Nav from "./components/Nav"
 import Main from "./components/Main"
 import About from "./components/About"
@@ -6,24 +9,37 @@ import Work from "./components/Work"
 import Contact from "./components/Contact"
 import NotFound from "./components/NotFound"
 import Footer from "./components/Footer"
-// import './App.css'
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      text: {
+        primary: "#333333"
+      },
+      background: {
+        default: "#ccccff",
+      },
+    },
+  })
+
   return (
-    <>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
   )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { ThemeProvider, createTheme } from "@mui/material/styles"
-import './index.css';
 import App from './App';
 
-// const theme = createTheme({
-//     palette: {
-//       primary: {
-//         main: "#ccccff",
-//       },
-//       secondary: {
-//         main: '#333333',
-//       },
-//     },
-//   })
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    // <ThemeProvider theme={theme}>
-        <App />
-    // </ThemeProvider>
-)
+root.render(<App />)
