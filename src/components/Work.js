4afce11829d8c9b09d728ea41f58653d168ee6b4
Martin <souzam@sonic.net>
2024-01-03 12:41:47 -0500

    Added project data file

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,11 +1,12 @@
-import { Container, Box, Button } from "@mui/material"
+import { Container, Box, Button, Typography } from "@mui/material"
 import FileDownloadIcon from "@mui/icons-material/FileDownload"
 import martinsouza_resume from "../res/martinsouza_resume.pdf"
+import projectData from "../res/projectData"
 
 function Work() {
   return (
     <Container>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: "flex", justifyContent: "center", marginBottom: 3 }}>
         <Button
           variant="contained"
           size="large"
@@ -16,6 +17,8 @@ function Work() {
           Martin's résumé (PDF)
         </Button>
       </Box>
+
+      <Typography variant="h4">Technical Projects</Typography>
     </Container>
   )
 }
