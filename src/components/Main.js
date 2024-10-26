90aa1d2e97f557aebd60d673d05b1dc0a5cccf66
Martin <souzam@sonic.net>
2024-10-26 15:06:25 -0400

    Updated Services

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ function Main() {
     <Container>
       <Stack direction={{ xs: "column", sm: "row"}} spacing={3}>
         <Typography variant="body">
-          I became a Polaroid photographer when my father gave me his SX-70 in 2022. As an inquisitive beginner with no technical knowledge of photography, I approached the Polaroid medium on its own accessible and intuitive terms, and quickly came to love it for its immediacy, unpredictability, and imperfection. I'm committed to sharing knowledge and experience, and encouraging others to explore and experiment with instant photography, whatever their background in other photography may be.
+          I became a Polaroid photographer when my father gave me his SX-70 in 2022. As an inquisitive beginner with no technical knowledge of photography, I approached the Polaroid medium on its own accessible and intuitive terms, and quickly came to love it for its immediacy, unpredictability, and imperfection. I'm committed to sharing knowledge and experience, and encouraging others to explore and experiment with instant photography, whatever their other artistic or photographic background may be.
           <br/>
           <br/>
           In addition to my services as a specialist Polaroid photographer, I offer introductory instruction in the medium through my Polaroid 101 workshops and in private sessions. I also provide vintage Polaroid cameras for rental, including whatever level of guidance and support you need to use them.
