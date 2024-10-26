faa9593825010a6efb8a7d2a1f28c2014c160207
Martin <souzam@sonic.net>
2024-10-26 17:21:11 -0400

    Updated Contact, implemented contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,46 @@
-import { Container, Typography, Box, Stack, Divider, Button } from "@mui/material"
+import { useState } from "react"
+import { Container, Typography, Box, Stack, Divider, Button, Input } from "@mui/material"
 import InstaIcon from '@mui/icons-material/Instagram'
 import CropOriginalSharpIcon from '@mui/icons-material/CropOriginalSharp'
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
 
 function Contact() {
+  const [result, setResult] = useState(null)
+  
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    setResult("Sending...")
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "1f0a0bfc-01ad-4401-924b-64003534bf6e")
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json()
+
+    if (data.success) {
+      setResult("Form Submitted Successfully")
+      event.target.reset()
+    } else {
+      console.log("Error", data)
+      setResult(data.message)
+    }
+  }
+
   return (
     <Container>
       <Typography variant="body">
-        For professional inquiries, please contact me via e-mail.
-        <br/>
-        <br/>
         You can find me out and about online:
       </Typography>
-      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2}}>
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
         <Stack
           direction={{ xs: 'column', sm: 'row' }}
           spacing={{ xs: 1.5, sm: 2}}
           divider={<Divider orientation="vertical" flexItem />}
         >
-          {/* <Button
-            variant="text"
-            color="inherit"
-            size="large"
-            startIcon={<EmailOutlinedIcon />}
-            href="https://dev.to/souzamartin"  
-            target="_blank"
-            rel="noopener"
-            sx={{ backgroundColor: "primary.light" }}
-          >
-            e-mail
-          </Button> */}
           <Button
             variant="text"
             color="inherit"
@@ -55,6 +66,55 @@ function Contact() {
             The Instant Film Magazine
           </Button>
         </Stack>
+      </Box>
+      <Box sx={{ marginTop: 3 }}>
+        <Typography variant="body">
+          For professional inquiries, please contact me via e-mail:
+        </Typography>
+        <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
+          {result ? 
+            <Typography>{result}</Typography>
+          :
+            <form onSubmit={onSubmit}>
+              <Input
+                type="text"
+                name="name"
+                placeholder="Name"
+                required
+                variant="standard"
+              />
+              <br/>
+              <Input
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                required
+                variant="standard"
+                sx={{ marginTop: 2}}
+              />
+              <Input 
+                name="message"
+                placeholder="Message"
+                required
+                fullWidth
+                multiline
+                variant="standard"
+                sx={{ marginTop: 2}}
+              >
+              </Input>
+              <Button
+                type="submit"
+                variant="text"
+                color="inherit"
+                size="large"
+                startIcon={<EmailOutlinedIcon />}
+                sx={{ backgroundColor: "primary.light", marginTop: 2 }}
+              >
+                Send
+              </Button>
+            </form>
+          }
+        </Box>
       </Box>
     </Container>
   )
